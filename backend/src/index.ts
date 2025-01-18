e2b36fd1cdd889d16b26050775357e9807fac1fb
vijaykolar <vijayikolar@gmail.com>
2025-01-18 12:42:22 +0530

    Implement environment configuration and session management

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,14 +1,44 @@
+import dotenv from 'dotenv';
+dotenv.config();
 import express from 'express';
 import morgan from 'morgan';
+import session from 'cookie-session';
+import cors from 'cors';
+
+import { config } from './config/app-config';
+
+//
+const BASE_PATH = config.BASE_PATH;
 
 const app = express();
 
+// Middlewares
 app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  session({
+    name: 'session',
+    keys: [config.SESSION_SECRET],
+    maxAge: 24 * 60 * 60 * 1000,
+    secure: config.NODE_ENV === 'production',
+    httpOnly: true,
+    sameSite: 'lax',
+  }),
+);
+app.use(
+  cors({
+    origin: config.FRONTEND_ORIGIN,
+    credentials: true,
+  }),
+);
 
+// Routes
 app.get('/', (req, res) => {
-  res.send('Hello World!!!!@');
+  res.send('Hello World');
 });
 
-app.listen(3000, () => {
-  console.log('Server is running on http://localhost:3000');
+// Server
+app.listen(config.PORT, () => {
+  console.log(`Server is running on ${config.PORT} in ${config.NODE_ENV} mode`);
 });
