0a0edb11f921f9eb0334abef23fb5283e2f79011
vijaykolar <vijayikolar@gmail.com>
2025-01-24 10:15:29 +0530

    Refactor ESLint configuration for consistency, update CreateWorkspaceDialog to remove unused title, and enhance ProtectedRoute with loading state and user authentication check

--- frontend/src/components/workspace/create-workspace-dialog.tsx
+++ frontend/src/components/workspace/create-workspace-dialog.tsx
@@ -1,13 +1,12 @@
 import WorkspaceForm from "./create-workspace-form";
 import useCreateWorkspaceDialog from "@/hooks/use-create-workspace-dialog";
 import { Dialog, DialogContent } from "@/components/ui/dialog";
-import { DialogTitle } from "@radix-ui/react-dialog";
+
 const CreateWorkspaceDialog = () => {
   const { open, onClose } = useCreateWorkspaceDialog();
 
   return (
     <Dialog modal={true} open={open} onOpenChange={onClose}>
-      <DialogTitle>ddd</DialogTitle>
       <DialogContent
         aria-describedby="Create work space"
         className="sm:max-w-5xl !p-0 overflow-hidden border-0"
--- frontend/src/routes/protected.route.tsx
+++ frontend/src/routes/protected.route.tsx
@@ -1,5 +1,14 @@
-import { Outlet } from "react-router-dom";
+import { DashboardSkeleton } from "@/components/skeleton-loaders/dashboard-skeleton";
+import useAuth from "@/hooks/api/use-auth";
+import { Navigate, Outlet } from "react-router-dom";
 const ProtectedRoute = () => {
+  const { data: authData, isLoading } = useAuth();
+  const user = authData?.user;
+
+  if (isLoading) return <DashboardSkeleton />;
+
+  if (!user) return <Navigate to="/login" replace />;
+
   return <Outlet />;
 };
 
